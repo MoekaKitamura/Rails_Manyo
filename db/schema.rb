2969d38dfd6708c4485c7835d9b19c7c15e8d2cd
moeka kitamura <moeka.k76@gmail.com>
2021-08-26 09:35:14 +0900

    add index to task_name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_23_055036) do
+ActiveRecord::Schema.define(version: 2021_08_25_235016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_08_23_055036) do
     t.string "priority"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["task_name"], name: "index_tasks_on_task_name"
   end
 
 end
